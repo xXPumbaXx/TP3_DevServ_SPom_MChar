a1fe7dcc9b7b6b071b8b213ac657dbf3c3480ac0
mcharette-SF <1844680@csfoy.ca>
2024-05-26 11:22:45 -0400

    Correction Lighthouse

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable
 {
