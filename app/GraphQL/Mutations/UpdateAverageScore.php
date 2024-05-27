1af5c4712ea42a75f8e1999b6bfddb17f5961fd9
mcharette-SF <1844680@csfoy.ca>
2024-05-26 23:42:54 -0400

    final commit

--- app/GraphQL/Mutations/UpdateAverageScore.php
+++ app/GraphQL/Mutations/UpdateAverageScore.php
@@ -10,26 +10,32 @@ final readonly class UpdateAverageScore
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
-        // TODO implement the resolver
-        $retval = 0;
+        $new_score = 0;
+        $new_votes = 0;
         try
         {
-            User::findOrFail($args["user_id"]);
+            User::findOrFail($args["user_id"]); // Safety
+
             $critic_film = CriticFilm::findOrFail($args["film_id"]);
             
+            $score = ($critic_film->score * $critic_film->votes) + $args["score"];
             $votes = $critic_film->votes + 1;
-            $score = $critic_film->score + $args["score"];
 
+            $critic_film->score = $score / $votes;
             $critic_film->votes = $votes;
-            $critic_film->score += $score / $votes;
             $critic_film->save();
 
-            $retval = $critic_film->score;
+            $new_score = number_format($critic_film->score, 2, '.', '');
+            $new_votes = $votes;
         }
         catch(Exception $ex)
         {
             abort(422, 'Invalid mutation');
         }
-        return $retval; #"ta mere"; #$args["id"];
+        return array(
+            "score" => $new_score,
+            "votes" => $new_votes,
+            "film_id" => $args["film_id"]
+        );
     }
 }
